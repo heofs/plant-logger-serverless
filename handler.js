b1c3dae901934ddf14fd50ecf416bbee0a62e488
Henning Ofstad <ofstad10@gmail.com>
2019-05-04 13:17:05 +0100

    Initial commit

---
+++ handler.js
@@ -0,0 +1,27 @@
+//handler.js
+
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const { userTypeDef, userResolvers } = require("./src/models/user");
+
+const typeDefs = gql`
+  ${userTypeDef}
+`;
+const resolvers = userResolvers;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context
+  })
+});
+
+exports.graphql = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
